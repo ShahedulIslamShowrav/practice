0a24e2c02e799c6f9603d1984bcf7c665afd9a9e
Show rav <showrav.info@gmail.com>
2023-03-05 20:14:58 +0600

    Change titles and styling on homepage

---
+++ src/App.vue
@@ -0,0 +1,177 @@
+<template>
+ <header>
+  <main>
+    <img src="" alt="">
+    <section>
+      <nav>
+        <a href="">Abot Me</a>
+        <a href="">Fashion</a>
+        <a href="">Food</a>
+        <a href="">Lifestyle</a>
+        <a href="">Contact Me</a>
+      </nav>
+    </section>
+
+  </main>
+ </header>
+ <body>
+  <main>
+    <section>
+      <h1>
+        I am
+      </h1>
+      <b>
+        Hailey Martin
+      </b>
+    </section>
+    <br>
+    <section>
+      <span>
+        I am a Fashion, Food and Lifestyle Blogger.
+      </span>
+      <br>
+      <p>
+        Loren ipsum dolor sit amet, consectetur adipiscing elit. ut elit tellus,luctus nec ullamcorper mattis, pulvinar dapibus leo.
+      </p>
+    </section>
+    <section>
+      <div>
+        <img src="" alt="">
+        <b>
+          Ultimate Shopping Guide For Every Girl
+        </b>
+        <br>
+        <span>
+          Fashion / By harshadd / 1 minute of reading
+        </span>
+        <br>
+        <p>
+          Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris. Sinistra possedit litora ut nabataeaque. Setucant coepyterunt perveniunt animal! Concordi aurea …
+        </p>
+        <a href="">read More</a>
+      </div>
+      <hr>
+      <div>
+        <img src="" alt="">
+        <b>
+          Ultimate Shopping Guide For Every Girl
+        </b>
+        <br>
+        <span>
+          Fashion / By harshadd / 1 minute of reading
+        </span>
+        <br>
+        <p>
+          Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris. Sinistra possedit litora ut nabataeaque. Setucant coepyterunt perveniunt animal! Concordi aurea …
+        </p>
+        <a href="">read More</a>
+      </div>
+      <hr>
+      <div>
+        <img src="" alt="">
+        <b>
+          Ultimate Shopping Guide For Every Girl
+        </b>
+        <br>
+        <span>
+          Fashion / By harshadd / 1 minute of reading
+        </span>
+        <br>
+        <p>
+          Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris. Sinistra possedit litora ut nabataeaque. Setucant coepyterunt perveniunt animal! Concordi aurea …
+        </p>
+        <a href="">read More</a>
+      </div>
+      <hr>
+      <div>
+        <img src="" alt="">
+        <b>
+          Ultimate Shopping Guide For Every Girl
+        </b>
+        <br>
+        <span>
+          Fashion / By harshadd / 1 minute of reading
+        </span>
+        <br>
+        <p>
+          Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris. Sinistra possedit litora ut nabataeaque. Setucant coepyterunt perveniunt animal! Concordi aurea …
+        </p>
+        <a href="">read More</a>
+      </div>
+      <hr>
+      <div>
+        <img src="" alt="">
+        <b>
+          Ultimate Shopping Guide For Every Girl
+        </b>
+        <br>
+        <span>
+          Fashion / By harshadd / 1 minute of reading
+        </span>
+        <br>
+        <p>
+          Fuerat aestu carentem habentia spectent tonitrua mutastis locavit liberioris. Sinistra possedit litora ut nabataeaque. Setucant coepyterunt perveniunt animal! Concordi aurea …
+        </p>
+        <a href="">read More</a>
+      </div>
+      <hr>
+    </section>
+    <br>
+    <section>
+      <button>
+        1
+      </button>
+      <button>
+        2
+      </button>
+      <a href="">Next page</a>
+    </section>
+  </main>
+  <hr>
+</body>
+<footer>
+  <section>
+  <h1>
+    f
+  </h1>
+  <h1>
+    t
+  </h1>
+  <h1>
+    i
+  </h1>
+  <h1>
+    w
+  </h1>
+</section>
+<section>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+  </p>
+  <button>
+    Contact
+  </button>
+<br>
+  <span>
+    Copyright © 2023 Personal Blog | Powered by Personal Blog
+  </span>
+</section>
+</footer>
+</template>
+
+<script>
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
